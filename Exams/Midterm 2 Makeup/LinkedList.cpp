8d0a42a80128abfdc2828993ab63ec6a5bc7cd89
MichaelMerola <michaelr.merola@gmail.com>
2017-03-15 23:08:03 -0600

    Exam Updated

--- Exams/Midterm 2 Makeup/LinkedList.cpp
+++ Exams/Midterm 2 Makeup/LinkedList.cpp
@@ -20,19 +20,55 @@ class LinkedList{
 			Node *x = head;
 			while(x != NULL){
 				if(x->key == value){
-					break;
+					return x; //changed
 				}else{
 					x = x->next;
 				}
 			}
-			return x;
+			return NULL;//changed
 		}
 
 	public:
 
 		LinkedList(){head = NULL;};
 		void addNode(int value, int newVal){
-            //your code here
+
+			Node *place; //node that will be previous to new
+		    Node *right;
+			Node *add; //new node
+
+			place = search(value);
+			if (place != NULL) {
+		    	right = place->next;
+			}
+
+		    add = new Node(newVal, NULL, NULL);
+
+		    if (head == NULL) { //check if head is NULL
+		        head = add;
+		        head->next = NULL;
+		        head->previous = NULL;
+		    }//end if
+			else if (place == NULL) { //value isn't found
+				add->next = head;
+				head->previous = add;
+				head = add;
+			}
+		    else { //place is valid and list has head
+
+	            if (place->next == NULL) { //check if place is tail
+	                place->next = add;
+					add->previous = place;
+	            }
+	            else {
+					place->next = add;
+					add->next = right;
+
+					right->previous = add;
+					add->previous = place;
+	            }
+
+		    }//end else
         }
 
 		void printList(Node *h){
--- Exams/Midterm 2 Makeup/midterm2.cpp
+++ Exams/Midterm 2 Makeup/midterm2.cpp
@@ -30,11 +30,11 @@ void printList(Node *head)
 /*Exam question:
 Add a method to the LinkedList.cpp file to add a node to a linked list.
 The function definition is provided, you need to fill in where it says
-"your code here". The function takes two arguments - the value of the 
+"your code here". The function takes two arguments - the value of the
 previous node and the value of the new node to add. For example, if you
 call addNode(5, 6), it means add a node to the linked list with a value
 of 6 after the node with a value of 5. If the previous value does not exist
-in the list, add the new node to the head of the list. You are working with a 
+in the list, add the new node to the head of the list. You are working with a
 doubly linked list.
 
 Test cases:
@@ -49,28 +49,28 @@ Value found - add after as new end of the list
 int main(){
 	LinkedList ll;
 	Node *head;
-	//ll.addNode(1, 5);  // 5->
-	//ll.addNode(5, 10); // 5->10
-	//ll.addNode(5,6); //5->6->10
-	
-	
-	
+	ll.addNode(1, 5);  // 5->
+	ll.addNode(5, 10); // 5->10
+	ll.addNode(5,6); //5->6->10
+
+
+
 	cout<<"Test Case 1\n==========\n"; //Expected Output is 5 6 10 and 10 6 5
 	head=ll.getHead();
-	
+
 	printList(head);
-	
-	//Expected Output is 5 6 10 and 10 6 5
-	
-	
-	
+
+//Expected Output is 5 6 10 and 10 6 5
+
+
+
 	cout<<"\n\nTest Case 2 \n==========\n"; //Expected Output 12 5 6 7 10 11
-	//ll.addNode(6,7); //5->6->7->10
-	//ll.addNode(10, 11); //5->6->7->10->11
-	//ll.addNode(100, 12); //12->5->6->7->10->11 (As there is no 100 insert 100 in the head position)
+	ll.addNode(6,7); //5->6->7->10
+	ll.addNode(10, 11); //5->6->7->10->11
+	ll.addNode(100, 12); //12->5->6->7->10->11 (As there is no 100 insert 100 in the head position)
 	head=ll.getHead();
     printList(head);
-	
-//Expected Output 12 5 6 7 10 11 and 11 10 7 6 5 12 
+
+//Expected Output 12 5 6 7 10 11 and 11 10 7 6 5 12
 
  }
